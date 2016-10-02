fab3d068f21629c7905d5f113135160f9c4c7c47
mudrakpatel <mudrakpatel96@gmail.com>
2016-10-02 15:12:08 -0500

    Initial commit

---
+++ src/Ex1/Question.java
@@ -0,0 +1,14 @@
+package Ex1;
+
+public class Question {
+    //Define the question
+    public static String question;
+
+    //methods and constructors
+    public Question(String question){
+        this.question=question;
+    }
+
+    //simulateQuestion method
+    
+}
